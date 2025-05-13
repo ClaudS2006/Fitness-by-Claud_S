a6da2ec572ada00665f3d7121a023434169dba3d
ClaudS2006 <165497828+ClaudS2006@users.noreply.github.com>
2025-05-13 16:30:54 +0200

    submit link back to button
    split js into 3 different scripts for better maintenance
    button submit works better for actual submit trigger

---
+++ fitness.js
@@ -0,0 +1,30 @@
+// SEND MESSSAGE
+
+const form = document.getElementById("contact-form");
+const output = document.getElementById("confirmation-output");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault(); // prevents page reload
+
+  if (!form.checkValidity()) {
+    // if validation fails
+    output.textContent = "Please fill in all required fields correctly.";
+    output.style.color = "red";
+    return;
+  }
+
+  const name = form.name.value.trim();
+  const email = form.email.value.trim();
+  const message = form.message.value.trim();
+
+  const subject = encodeURIComponent("New message from " + name);
+  const body = encodeURIComponent(`Name: ${name}\nEmail: ${email}\n\n${message}`);
+  window.location.href = `mailto:cstoll2006@gmail.com?subject=${subject}&body=${body}`;
+
+  output.textContent = "Your message has been passed to your email provider.";
+  output.style.color = "green";
+
+  setTimeout(() => {
+    output.textContent = "";
+  }, 5000);
+});
