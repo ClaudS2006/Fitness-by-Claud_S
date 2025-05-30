4f66b8a2f5a4e29c906c70b6445e4b7d2eb58823
ClaudS2006 <165497828+ClaudS2006@users.noreply.github.com>
2025-05-30 18:48:16 +0200

    fitness.js
    timeout to show message before it gets send

--- fitness.js
+++ fitness.js
@@ -1,17 +1,28 @@
-// SEND MESSSAGE via formsubmit 
+// SEND MESSSAGE via formsubmit
 
 const form = document.getElementById("contact-form");
 const output = document.getElementById("confirmation-output");
 
 form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
   if (!form.checkValidity()) {
     e.preventDefault();
     output.textContent = "Please fill in all required fields correctly.";
     output.style.color = "red";
     return;
   }
-  
+
   // show status before sending
   output.textContent = "Sending your message...";
   output.style.color = "blue";
+  // timing to show messages
+  setTimeout(() => {
+    output.textContent = "Your message has been sent successfully!";
+    output.style.color = "green";
+
+    setTimeout(() => {
+      form.submit(); // form sent
+    }, 2000);
+  }, 1000);
 });
